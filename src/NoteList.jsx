f65e4b5e19a75e6199b54e076f23df96c75c3dc0
stahnu <shota.horinouchi@gmail.com>
2026-08-19 19:46:12 +0900

    単一コンポーネントの外側のフラグメントを削除

--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -5,12 +5,10 @@ export function NoteList({ onSelect }) {
   const notes = useNotes();
 
   return (
-    <>
-      <ul>
-        {notes.map((note) => (
-          <NoteListItem key={note.id} note={note} onSelect={onSelect} />
-        ))}
-      </ul>
-    </>
+    <ul>
+      {notes.map((note) => (
+        <NoteListItem key={note.id} note={note} onSelect={onSelect} />
+      ))}
+    </ul>
   );
 }
